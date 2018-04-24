447cbb5cbffd234cc3df97943485bc0d2bbd2f7f
tearsofphoenix <tearsofphoenix@icloud.com>
2018-04-24 15:07:28 +0800

    file preview ok

--- renderer/containers/FileSystem/FileInspector.js
+++ renderer/containers/FileSystem/FileInspector.js
@@ -1,3 +1,54 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
+import { connect } from 'dva'
+import moment from 'moment'
 
-export default () => (<div />)
+@connect(({ file}) => ({ file }))
+export default class Inspector extends PureComponent {
+  render() {
+    const {file: {selected}} = this.props
+    if (selected.NSURLNameKey) {
+      return (<div className="item-views">
+        <div className="styleguide pane-item">
+          <header className="styleguide-header">
+            <h5>File Inspector</h5>
+          </header>
+          <main className="styleguide-sections">
+            <section className="bordered">
+              <h3>File Info</h3>
+              <div className="control">
+                <div className="control-rendered">
+                  <div className="block" style={{display: 'flex'}}>
+                    <div className="control-wrap">
+                      <div className="label">Name</div>
+                      <div>{selected.NSURLNameKey}</div>
+                    </div>
+                  </div>
+                  <div className="block" style={{display: 'flex'}}>
+                    <div className="control-wrap">
+                      <div className="label">Path</div>
+                      <div>{selected._NSURLPathKey}</div>
+                    </div>
+                  </div>
+                  <div className="block" style={{display: 'flex'}}>
+                    <div className="control-wrap">
+                      <div className="label">Size</div>
+                      <div>{selected.NSURLFileSizeKey}</div>
+                    </div>
+                  </div>
+                  <div className="block" style={{display: 'flex'}}>
+                    <div className="control-wrap">
+                      <div className="label">Create Date</div>
+                      <div>{moment(selected.NSURLCreationDateKey).format('YYYY-MM-DD HH:mm:ss')}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
+      </div>)
+    } else {
+      return (<div className="empty">No Selection</div>)
+    }
+  }
+}
